2d144a7ad972c32c09330ca3b47e45f8c17f526a
Cloud User <ec2-user@ip-172-31-40-88.ap-south-1.compute.internal>
2019-06-13 15:20:21 +0000

    Sender and reciver work message not in a loop file sending working

--- Python/problem10-client.py
+++ Python/problem10-client.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python36
+#!/usr/bin/python2
 import socket
 import time
 #this_is_reciver(Client)                                                                                
-port=9999
-ip=input("Enter server ip")
+port=10000
+ip=raw_input("Enter server ip")
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind((ip,port))
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
 #sending message to send reciver ip
-s.sendto(str.encode("Hi there"),ip)
-option=input("Enter option")
+s.sendto("Hi there",(ip,port))
+option=raw_input("Enter option  1/2  ")
 if option=='1':
-	what=input("Press S to send and R to recive")
+	what=raw_input("Press S to send and R to recive")
 	if what=='S':
-		message=input()
-		s.sendto(str.encode(message),(reciver)) 
+		message=raw_input("Enter message : ")
+		s.sendto(message,(ip,port)) 
 	else:
-		reci=str.decode(s.recvfrom(1000))
+		reci=s.recvfrom(1000)
 		print("Reciver: "+reci[0])
 elif option=='2':
 	length=s.recvfrom(100)
@@ -26,4 +23,5 @@ elif option=='2':
 	data=s.recvfrom(length)
 	new=open('recived.txt','w')
 	new.write(data[0])
+s.close()
 exit()  
