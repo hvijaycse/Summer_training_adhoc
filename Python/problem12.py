906f3d1d2c2eae3f68668c1fdb31023c623cb226
Cloud User <ec2-user@ip-172-31-40-88.ap-south-1.compute.internal>
2019-06-14 06:08:29 +0000

    Now also uploding qr code on webpage as well

--- Python/problem12.py
+++ Python/problem12.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python36
 import qrcode
 from googlesearch import search
+html=open("/var/www/html/a.html",'a')
 data=input("Enter Search\t")
 urls=search(data,stop=3)
 i=1
 for url in urls:
 	image=qrcode.make(url)
-	with open(data+str(i)+".png",'wb') as f:
+	with open("/var/www/html/"+data+str(i)+".png",'wb') as f:
 		image.save(f)
 	i+=1
+	html.write('<h1>'+data+"</h1>")
+	html.write("<img src="+'"'+data+str(i)+".png"+'">'+"\n")
 print("qrcode made")
 exit()	
