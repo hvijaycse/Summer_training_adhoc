bed2e4c7b6ee097127d7c8cd351eea44ef8a45ca
Harsh Vijay <harshvijay.cse21@jecrcj.ac.in>
2019-06-22 10:39:41 +0530

    New Branch

---
+++ pynumber1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random
+
+list=[]
+shape=input("Please Enter shape of array in aXb format").split('X')
+for  i in range(int(shape[0])):
+  temp_list=[]
+  for t in range(int(shape[1])):
+    temp_list.append(random.randrange(1,200))
+  list.append(temp_list)
+
+array=np.array(list)
+
+print(array)
+
+store=open('array.txt','w')
+store.write(str(array))
+store.close()
